6bed8a0548051ba3e7168591284598b63503648e
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-10 16:02:19 +0800

    feat: Add dummy data tasks for layouts item

---
+++ src/data/dummy_data.ts
@@ -0,0 +1,37 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      judul: "Tugas Matdas",
+      deskripsi:
+        "Ini adalah deskripsi tugas Matdas. Ini adalah deskripsi tugas Matdas woyy. Ini adalah deskripsi tugas Matdas iyee.",
+      deadline: "5 Juni 2023",
+    },
+    {
+      id: 2,
+      judul: "Tugas PBO",
+      deskripsi: "Ini adalah deskripsi tugas PBO",
+      deadline: "6 Juni 2023",
+    },
+    {
+      id: 3,
+      judul: "Tugas TJW",
+      deskripsi: "Ini adalah deskripsi tugas TJW",
+      deadline: "7 Juni 2023",
+    },
+    {
+      id: 4,
+      judul: "Tugas PKN",
+      deskripsi: "Ini adalah deskripsi tugas PKN",
+      deadline: "7 Mei 2023",
+    },
+    {
+      id: 5,
+      judul: "Tugas Bahasa Inggris",
+      deskripsi: "Ini adalah deskripsi tugas Bahasa Inggris",
+      deadline: "20 April 2023",
+    },
+  ];
+};
+
+export { getData };
