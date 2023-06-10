c1892c691fb455a1b5416466e7efd1525bfef4d4
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-10 16:14:50 +0800

    feat: Call all pages file in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Add from "./pages/Add";
+import { getData } from "./data/dummy_data";
 
-function App() {
-  const [count, setCount] = useState(0)
+const data = getData();
+
+const App = () => {
+  const [tasks, setTask] = useState(data);
+
+  const handleClickAddTask = (
+    judul: string,
+    deskripsi: string,
+    deadline: string
+  ) => {
+    const newTask = {
+      id: +new Date(),
+      judul,
+      deskripsi,
+      deadline,
+    };
+    setTask((prev) => [...prev, newTask]);
+  };
+
+  const handleDeleteTask = (id: number) => {
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    setTask(filteredTasks);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='font-bold'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routers>
+      <Routes>
+        <Route path="/" element={<Home data={tasks} handleDeleteTask={handleDeleteTask} />} />
+        <Route
+          path="/add"
+          element={<Add handleAddTask={handleClickAddTask} />}
+        />
+      </Routes>
+    </Routers>
+  );
+};
 
-export default App
+export default App;
